e6be413193259a12043d44e682837a08b4d2c0b5
Izbrodin Alexey <izbrodin90@gmail.com>
2018-07-20 23:33:32 +0300

    Refactoring

--- L031collections/src/main/java/ru/otus/I031/Main.java
+++ L031collections/src/main/java/ru/otus/I031/Main.java
@@ -9,12 +9,14 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> integerList = new MyArrayList<>();
 
-        Collections.addAll(integerList, 2, 4 ,- 1, 0, 13);
+        Collections.addAll(integerList, 2, 4, -1, 0, 13);
 
-        List<Integer>copyList = new MyArrayList<>(integerList.size());
+        List<Integer> copyList = new MyArrayList<>(integerList.size());
 
         Collections.copy(copyList, integerList);
         Collections.sort(copyList, Comparator.reverseOrder());
+
+//        Test
 //        for (int i=0; i < copyList.size();i++){
 //            System.out.println(copyList.get(i));
 //        }
