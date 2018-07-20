e189803ae9b408bc327566a1433f56a4c37dcb9c
Izbrodin Alexey <izbrodin90@gmail.com>
2018-07-20 23:16:33 +0300

    Create Main.class

---
+++ L031collections/src/main/java/ru/otus/I031/Main.java
@@ -0,0 +1,22 @@
+package ru.otus.I031;
+
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> integerList = new MyArrayList<>();
+
+        Collections.addAll(integerList, 2, 4 ,- 1, 0, 13);
+
+        List<Integer>copyList = new MyArrayList<>(integerList.size());
+
+        Collections.copy(copyList, integerList);
+        Collections.sort(copyList, Comparator.reverseOrder());
+//        for (int i=0; i < copyList.size();i++){
+//            System.out.println(copyList.get(i));
+//        }
+    }
+}
