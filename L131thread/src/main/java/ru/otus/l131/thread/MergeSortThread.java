8908a779e3b0314919ec25f3fac7e49ce094ca65
IzbrodinA <izbrodin90@gmail.com>
2018-11-06 13:07:24 +0400

    Add test

--- L131thread/src/main/java/ru/otus/l131/thread/MergeSortThread.java
+++ L131thread/src/main/java/ru/otus/l131/thread/MergeSortThread.java
@@ -6,71 +6,67 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-//@SuppressWarnings("ALL")
-public class MergeSortThread {
-    private static int SIZE_ARRAY = 19;
+@SuppressWarnings("ALL")
+public  class MergeSortThread {
+    private static int SIZE_ARRAY ;
     private static int SIZE_THREADS = 4;
     private static List<Thread> pool_Threads = new ArrayList<>();
     private static Integer countThread = 0;
-    static int[] array = new int[SIZE_ARRAY];
-    private static int SIZE_PART_ARRAY = Math.floorDiv(SIZE_ARRAY , SIZE_THREADS);
+    static int[] array ;
+    private static int SIZE_PART_ARRAY ;
 
-    public static void main(String[] args) {
 
-        Random random = new Random();
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(1000);
+    public synchronized static void sort(int[] arrays) {
+        array = arrays;
+        SIZE_ARRAY = arrays.length;
+        if (SIZE_ARRAY == 0){
+            return;
         }
 
-        System.out.println(Arrays.toString(array));
-
-        for (int i = 0; i < SIZE_THREADS; i++) {
-            pool_Threads.add(new Thread(() -> {
-                sort();
-            }));
+        if (SIZE_ARRAY < 4){
+            // Size very small;
+            Arrays.sort(array);
+            return;
         }
 
-        for (int i = 0; i < SIZE_THREADS; i++) {
-            pool_Threads.get(i).start();
-        }
+        SIZE_PART_ARRAY = SIZE_ARRAY % SIZE_THREADS != 0 ? SIZE_ARRAY / SIZE_THREADS + 1 : SIZE_ARRAY / SIZE_THREADS;
 
-        for (int i = 0; i < SIZE_THREADS; i++) {
-            try {
-                pool_Threads.get(i).join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            for (int i = 0; i < SIZE_THREADS; i++) {
+                pool_Threads.add(new Thread(() -> {
+                    sortPartArray();
+                }));
             }
-        }
-        System.out.println(SIZE_PART_ARRAY);
+            pool_Threads.forEach(x -> x.start());
+            pool_Threads.forEach(thread -> {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
 
-        merge(0, (SIZE_ARRAY / 2 - 1) / 2, SIZE_ARRAY / 2 - 1);
-        merge(SIZE_ARRAY / 2, SIZE_ARRAY / 2 + (SIZE_ARRAY - 1 - SIZE_ARRAY / 2) / 2, SIZE_ARRAY - 1);
-        merge(0, (SIZE_ARRAY - 1) / 2, SIZE_ARRAY - 1);
-        System.out.println(Arrays.toString(array));
 
+        merge(0, SIZE_PART_ARRAY - 1, SIZE_PART_ARRAY * 2 - 1);
+        merge(SIZE_PART_ARRAY * 2, SIZE_PART_ARRAY * 3 - 1, SIZE_ARRAY - 1);
+        merge(0, SIZE_PART_ARRAY * 2 - 1, SIZE_ARRAY - 1);
     }
 
-    static void merge(int low, int mid, int high) {
+    private static void merge(int low, int mid, int high) {
         int[] left = new int[mid - low + 1];
         int[] right = new int[high - mid];
 
-        // n1 is size of left part and n2 is size
-        // of right part
         int n1 = mid - low + 1, n2 = high - mid, i, j;
 
-        // storing values in left part
         for (i = 0; i < n1; i++)
             left[i] = array[i + low];
 
-        // storing values in right part
         for (i = 0; i < n2; i++)
             right[i] = array[i + mid + 1];
 
         int k = low;
         i = j = 0;
 
-        // merge left and right in ascending order
+
         while (i < n1 && j < n2) {
             if (left[i] <= right[j])
                 array[k++] = left[i++];
@@ -78,29 +74,32 @@ public class MergeSortThread {
                 array[k++] = right[j++];
         }
 
-        // insert remaining values from left
+
         while (i < n1) {
             array[k++] = left[i++];
         }
 
-        // insert remaining values from right
+
         while (j < n2) {
             array[k++] = right[j++];
         }
     }
 
-    private static void sort() {
+    private static void sortPartArray() {
         int sizePart;
+        int low;
+        int high;
 
         synchronized (countThread) {
             sizePart = countThread++;
         }
+            low = sizePart * (SIZE_PART_ARRAY);
+            high = (sizePart + 1) * (SIZE_PART_ARRAY);
 
-        int low = sizePart * (SIZE_ARRAY / SIZE_THREADS);
-        int high = (sizePart + 1) * (SIZE_ARRAY / SIZE_THREADS) ;
-        System.out.println(low + " " + high);
-
-        int[] sortPartArrays = Arrays.copyOfRange(array, low, high );
+        if (high > SIZE_ARRAY) {
+            high = SIZE_ARRAY;
+        }
+        int[] sortPartArrays = Arrays.copyOfRange(array, low, high);
         Arrays.sort(sortPartArrays);
         for (int i = low; i < high; i++) {
             array[i] = sortPartArrays[i - low];
---
+++ L131thread/src/main/test/java/ru/otus/l131/thread/MergeSortThreadTest.java
@@ -0,0 +1,101 @@
+package ru.otus.l131.thread;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class MergeSortThreadTest {
+
+   Random random;
+    @Before
+    public void setUp() throws Exception {
+        random = new Random();
+
+    }
+
+    @Test
+    public void checkNull() {
+        int[] array = new int[0];
+        int[] copyArray = Arrays.copyOf(array, array.length);
+        MergeSortThread.sort(array);
+        Arrays.sort(copyArray);
+        assertArrayEquals(copyArray, array);
+    }
+
+    @Test
+    public void checkArrayLength1() {
+        int[] array = new int[1];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(1000);
+        }
+        int[] copyArray = Arrays.copyOf(array, array.length);
+        MergeSortThread.sort(array);
+        Arrays.sort(copyArray);
+        assertArrayEquals(copyArray, array);
+    }
+
+    @Test
+    public void checkArrayLength4() {
+        int[] array = new int[4];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(1000);
+        }
+
+        int[] copyArray = Arrays.copyOf(array, array.length);
+        MergeSortThread.sort(array);
+        Arrays.sort(copyArray);
+        assertArrayEquals(copyArray, array);
+    }
+
+
+    @Test
+    public void checkArrayLength20() {
+        int[] array = new int[20];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(1000);
+        }
+        int[] copyArray = Arrays.copyOf(array, array.length);
+
+        MergeSortThread.sort(array);
+        Arrays.sort(copyArray);
+
+        assertArrayEquals(copyArray, array);
+    }
+
+    @Test
+    public void checkArrayLength19() {
+        int[] array = new int[19];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(1000);
+        }
+        int[] copyArray = Arrays.copyOf(array, array.length);
+
+        MergeSortThread.sort(array);
+        Arrays.sort(copyArray);
+
+        assertArrayEquals(copyArray, array);
+    }
+
+    @Test
+    public void checkArrayLength1000() {
+        int[] array = new int[1000];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(1000);
+        }
+        int[] copyArray = Arrays.copyOf(array, array.length);
+
+        MergeSortThread.sort(array);
+        Arrays.sort(copyArray);
+
+        assertArrayEquals(copyArray, array);
+    }
+}
